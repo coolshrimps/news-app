88c171f605ac1c51f046ef2224b470a812497d59
coolshrimps <65729686+coolshrimps@users.noreply.github.com>
2020-05-26 21:48:43 -0700

    Create myapp.config.js

---
+++ myapp.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    apps: [{
+            name: "myapp-server",
+            script: "./server/server.js",
+            watch: true,
+            env: {
+                "NODE_ENV": "development",
+            },
+            env_production: {
+                "NODE_ENV": "production"
+            }
+        },
+        {
+            name: "myapp-client",
+            script: "./client/server.js",
+            watch: true,
+            env: {
+                "NODE_ENV": "development",
+            },
+            env_production: {
+                "NODE_ENV": "production"
+            }
+        }
+    ]
+}
+© 2020 GitHub, Inc.
